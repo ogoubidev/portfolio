1879ffa7bc9ffe4d04f22fab6bfb2405c0e6cdab
ogoubidev <mouktadirouogoubi9@gmail.com>
2024-10-18 20:56:25 +0100

    initial project

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Code pour ajouter des interactions
+});
+
+function showAlert() {
+    alert("Merci d'avoir contacté Hubert AROBADI ADIMOU ! je vous réponds dans un instant ");
+}
